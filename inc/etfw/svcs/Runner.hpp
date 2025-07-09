31578fc8b8d6b55b966934ef9bb40b0beeda4c86
JKSMTH8 <jms564@pitt.edu>
2025-07-09 14:48:25 -0400

    interface for active runner. Allows dynamic task creation

--- inc/etfw/svcs/Runner.hpp
+++ inc/etfw/svcs/Runner.hpp
@@ -12,244 +12,166 @@
 
 namespace etfw {
 
-class iSvcRunner
-{
-    public:
-        enum class State_t
-        {
-            CREATED,        /**< Service object has been created. */
-            INITIALIZED,    /**< Service runner has been initialized. */
-            STARTING,       /**< Service has been requested to start. */
-            ACTIVE,         /**< Service has started an is running. */
-            STOP_REQUESTED, /**< Service has been requested to stop running. */
-            STOPPING,       /**< Service is stopping. */
-            STOPPED,        /**< Service has been stopped successfully. */
-            EXITED,         /**< Service has exited (initiated internally). */
-            ERROR,          /**< Service init, start, or stop has failed 
-                                 or has encountered an internal error. */
-        };
-        
-        enum class RunStatus
-        {
-            OK,
-            DONE,
-            ERROR,
-        };
-
-        iSvcRunner();
-
-        iSvcRunner(iSvc* svc);
-
-        virtual ~iSvcRunner();
-
-        virtual RunStatus start() = 0;
-
-        virtual RunStatus stop() = 0;
-
-        inline State_t state() const { return State; }
-
-        inline iSvc* svc() { return Svc; }
-
-        inline bool is_active(void) const
-        {
-            return ((State == State_t::STARTING) || 
-                    (State == State_t::ACTIVE));
-        }
-
-        inline bool is_stopped(void) const
-        {
-            return (!is_active());
-        }
-
-        void stop_children();
-
-    protected:
-        iSvc* Svc;
-        State_t State;
-
-        /// @brief Interface to Svc logger.
-        /// @param level Log severity level
-        /// @param format String to format and write
-        /// @param Args String format arguments
-        void log(const LogLevel level, const char* format, ...);
-};
-
-
-class PassiveRunner : public iSvcRunner
-{
-    public:
-        PassiveRunner(iSvc* svc);
-
-        RunStatus start() override;
-        RunStatus stop() override;
-};
-
-
-template <Os::Thread::Config::Priority_t TPriority, size_t TStackSz>
-class ActiveRunner : public iSvcRunner
-{
-    public:
-        using Priority_t = Os::Thread::Config::Priority_t;
-        using Stack_t = Os::Thread::Config::Stack::Buf_t;
-        using StackBuf_t = Os::Thread::Config::Stack::BufPtr_t;
-        static constexpr size_t StackSz = TStackSz;
-        static constexpr Priority_t Priority = TPriority;
-
-        ActiveRunner(iSvc* svc):
-            iSvcRunner(svc)
-        {}
-
-        /**
-         * @brief Verifies the current state and starts the service runner thread.
-         * 
-         * @return RunStatus 
-         */
-        RunStatus start() override
-        {
-            RunStatus run_status = RunStatus::ERROR;
-
-            if (State_t::CREATED == State ||
-                State_t::INITIALIZED == State ||
-                State_t::EXITED == State ||
-                State_t::STOPPED == State ||
-                State_t::ERROR == State)
+    class iSvcRunner
+    {
+        public:
+            enum class State_t
             {
-                Os::Thread::Routine_t routine = this->task_sm;
-                Os::Thread::RoutineArg_t arg = this;
-                Os::Thread::Config task_cfg(
-                    Stack_,
-                    StackSz,
-                    Priority,
-                    arg,
-                    routine
-                );
-                State = State_t::STARTING;
-                Os::Thread::Status os_stat = task_.start(task_cfg);
-                if (os_stat.error())
-                {
-                    State = State_t::ERROR;
-                }
-                else
-                {
-                    run_status = RunStatus::OK;
-                }
+                CREATED,        /**< Service object has been created. */
+                INITIALIZED,    /**< Service runner has been initialized. */
+                STARTING,       /**< Service has been requested to start. */
+                ACTIVE,         /**< Service has started an is running. */
+                STOP_REQUESTED, /**< Service has been requested to stop running. */
+                STOPPING,       /**< Service is stopping. */
+                STOPPED,        /**< Service has been stopped successfully. */
+                EXITED,         /**< Service has exited (initiated internally). */
+                ERROR,          /**< Service init, start, or stop has failed 
+                                    or has encountered an internal error. */
+            };
+            
+            enum class RunStatus
+            {
+                OK,
+                DONE,
+                ERROR,
+            };
+
+            iSvcRunner();
+
+            iSvcRunner(iSvc* svc);
+
+            virtual ~iSvcRunner();
+
+            virtual RunStatus start() = 0;
+
+            virtual RunStatus stop() = 0;
+
+            inline State_t state() const { return State; }
+
+            inline iSvc* svc() { return Svc; }
+
+            inline bool is_active(void) const
+            {
+                return ((State == State_t::STARTING) || 
+                        (State == State_t::ACTIVE));
             }
 
-            return run_status;
-        }
-
-        /**
-         * @brief Stops the 
-         * 
-         * @return RunStatus 
-         */
-        RunStatus stop() override
-        {
-            RunStatus status = RunStatus::DONE;
-
-            if (State_t::ACTIVE == State)
+            inline bool is_stopped(void) const
             {
-                State = State_t::STOP_REQUESTED;
-                status = RunStatus::OK;
+                return (!is_active());
             }
 
-            return status;
-        }
+            void stop_children();
 
-    private:
-        Stack_t Stack_[TStackSz];
-        Os::Thread task_;
+        protected:
+            iSvc* Svc;
+            State_t State;
 
-        static void task_sm(void* runner)
-        {
-            ETFW_ASSERT(runner != nullptr, "Null runner passed into task_sm");
-            ActiveRunner* runner_ = static_cast<ActiveRunner*>(runner);
-            task_sm_start(runner_);
-            task_sm_run(runner_);
-            task_sm_finish(runner_);
-        }
+            /// @brief Interface to Svc logger.
+            /// @param level Log severity level
+            /// @param format String to format and write
+            /// @param Args String format arguments
+            void log(const LogLevel level, const char* format, ...);
+    };
 
-        static void task_sm_start(ActiveRunner* task)
-        {
-            if (State_t::STARTING == task->State)
-            {
-                iSvc::RunStatus stat = task->Svc->pre_run_init();
-                if (iSvc::RunStatus::OK == stat)
-                {
-                    task->log(LogLevel::INFO,
-                        "Task context initialization complete. Starting active runner");
-                    task->State = State_t::ACTIVE;
-                }
-                else if (iSvc::RunStatus::DONE == stat)
-                {
-                    task->State = State_t::EXITED;
-                }
-                else
-                {
-                    task->State = State_t::ERROR;
-                }
-            }
-        }
 
-        static void task_sm_run(ActiveRunner* task)
-        {
-            task->log(LogLevel::INFO,
-                "ActiveRunner initialization complete. Starting task");
-            while (State_t::ACTIVE == task->State)
-            {
-                iSvc::RunStatus stat = task->Svc->process();
-                if (iSvc::RunStatus::DONE == stat)
-                {
-                    task->log(LogLevel::INFO,
-                        "Service returned DONE. Exiting.");
-                    task->stop_children();
+    class PassiveRunner : public iSvcRunner
+    {
+        public:
+            PassiveRunner(iSvc* svc);
 
-                    stat = task->Svc->post_run_cleanup();
-                    if (iSvc::RunStatus::OK == stat ||
-                        iSvc::RunStatus::DONE == stat)
-                    {
-                        task->log(LogLevel::INFO,
-                            "ActiveRunner exit cleanup complete. Service stopped.");
-                        task->State = State_t::EXITED;
-                    }
-                    else
-                    {
-                        // Failure
-                    }
-                }
-                else if (iSvc::RunStatus::ERROR == stat)
-                {
-                    task->State = State_t::ERROR;
-                }
-            }
-        }
+            RunStatus start() override;
+            RunStatus stop() override;
+    };
 
-        static void task_sm_finish(ActiveRunner* task)
-        {
-            if (State_t::STOP_REQUESTED == task->State)
-            {
-                task->log(LogLevel::INFO,
-                    "Stop requested. Exiting active runner service");
-                task->State = State_t::STOPPING;
-                task->stop_children();
+    class iActiveRunnerExt : public iSvcRunner
+    {
+        public:
+            using Priority_t = Os::Thread::Config::Priority_t;
+            using Stack_t = Os::Thread::Config::Stack::Buf_t;
+            using StackBuf_t = Os::Thread::Config::Stack::BufPtr_t;
 
-                /// TODO: need some sort of wait/async call back for children to stop
-                /// before calling cleanup
+            iActiveRunnerExt(iSvc* svc,
+                Priority_t priority,
+                const StackBuf_t stack_buf,
+                const size_t stack_sz
+            );
 
-                iSvc::RunStatus stat = task->Svc->post_run_cleanup();
-                if (iSvc::RunStatus::OK == stat ||
-                    iSvc::RunStatus::DONE == stat)
-                {
-                    task->State = State_t::STOPPED;
-                    task->log(LogLevel::INFO,
-                        "Service stopped");
-                }
-                else
-                {
-                    task->State = State_t::ERROR;
-                }
-            }
-        }
-};
+            /**
+             * @brief Verifies the current state and starts the service runner thread.
+             * 
+             * @return RunStatus 
+             */
+            RunStatus start() override;
+
+            /**
+             * @brief Stops the 
+             * 
+             * @return RunStatus 
+             */
+            RunStatus stop() override;
+
+        private:
+            Priority_t TaskPriority;
+            const StackBuf_t StackBuf;
+            const size_t StackSz;
+            Os::Thread task_;
+
+            /**
+             * @brief Main task state machine. Called in OS task.
+             * 
+             * @param runner 
+             */
+            static void task_sm(void* runner);
+
+            /**
+             * @brief Handles startup/initialization sequence for runner
+             * 
+             * @param task 
+             */
+            static void task_sm_start(iActiveRunnerExt* task);
+
+            /**
+             * @brief Main loop for service runner
+             * 
+             * @param task 
+             */
+            static void task_sm_run(iActiveRunnerExt* task);
+
+            /**
+             * @brief Handles cleanup when runner is stopped
+             * 
+             * @param task 
+             */
+            static void task_sm_finish(iActiveRunnerExt* task);
+            
+    };
+
+    template <size_t TStackSz>
+    class iActiveRunner : public iActiveRunnerExt
+    {
+        public:
+            using Base_t = iActiveRunnerExt;
+
+            iActiveRunner(iSvc* svc,
+                Priority_t priority
+            ):
+                Base_t(svc, priority, Stack, TStackSz)
+            {}
+
+        private:
+            Base_t::Stack_t Stack[TStackSz];
+    };
+
+    template <iActiveRunnerExt::Priority_t TPriority, size_t TStackSz>
+    class ActiveRunner : public iActiveRunner<TStackSz>
+    {
+        public:
+            using Base_t = iActiveRunner<TStackSz>;
+
+            ActiveRunner(iSvc* svc):
+                Base_t(svc, TPriority)
+            {}
+    };
 
 }
